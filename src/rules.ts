7821b290905c4a98af84ccfe7fcbb708f7013ba8
derkallevombau <kalle@Aspire2>
2020-05-03 16:55:22 +0200

    rules.ts: Improved formatting.

--- src/rules.ts
+++ src/rules.ts
@@ -2,40 +2,46 @@ import { IndentAction, OnEnterRule } from 'vscode';
 
 export class Rules
 {
-	static readonly multilineEnterRules: OnEnterRule[] = [
-		{
-			// e.g. /** | */
-			beforeText: /^\s*\/\*\*(?!\/)([^*]|\*(?!\/))*$/,
-			afterText: /^\s*\*\/$/,
-			action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
-		}, {
-			// e.g. /** ...|
-			beforeText: /^\s*\/\*\*(?!\/)([^*]|\*(?!\/))*$/,
-			action: { indentAction: IndentAction.None, appendText: ' * ' }
-		}, {
-			// e.g. /*! | */
-			beforeText: /^\s*\/\*!(?!\/)([^*]|\*(?!\/))*$/,
-			afterText: /^\s*\*\/$/,
-			action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
-		}, {
-			// e.g. /*! ...|
-			beforeText: /^\s*\/\*!(?!\/)([^*]|\*(?!\/))*$/,
-			action: { indentAction: IndentAction.None, appendText: ' * ' }
-		}, {
-			// e.g.  * ...|
-			beforeText: /^(\t|( ))* \*( ([^*]|\*(?!\/))*)?$/,
-			action: { indentAction: IndentAction.None, appendText: '* ' }
-		}, {
-			// e.g.  */|
-			beforeText: /^(\t|( ))* \*\/\s*$/,
-			action: { indentAction: IndentAction.None, removeText: 1 }
-		},
-		{
-			// e.g.  *-----*/|
-			beforeText: /^(\t|( ))* \*[^/]*\*\/\s*$/,
-			action: { indentAction: IndentAction.None, removeText: 1 }
-		}
-	];
+	static readonly multilineEnterRules: OnEnterRule[] =
+		[
+			{
+				// e.g. /** | */
+				beforeText: /^\s*\/\*\*(?!\/)([^*]|\*(?!\/))*$/,
+				afterText: /^\s*\*\/$/,
+				action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
+			},
+			{
+				// e.g. /** ...|
+				beforeText: /^\s*\/\*\*(?!\/)([^*]|\*(?!\/))*$/,
+				action: { indentAction: IndentAction.None, appendText: ' * ' }
+			},
+			{
+				// e.g. /*! | */
+				beforeText: /^\s*\/\*!(?!\/)([^*]|\*(?!\/))*$/,
+				afterText: /^\s*\*\/$/,
+				action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
+			},
+			{
+				// e.g. /*! ...|
+				beforeText: /^\s*\/\*!(?!\/)([^*]|\*(?!\/))*$/,
+				action: { indentAction: IndentAction.None, appendText: ' * ' }
+			},
+			{
+				// e.g.  * ...|
+				beforeText: /^(\t|( ))* \*( ([^*]|\*(?!\/))*)?$/,
+				action: { indentAction: IndentAction.None, appendText: '* ' }
+			},
+			{
+				// e.g.  */|
+				beforeText: /^(\t|( ))* \*\/\s*$/,
+				action: { indentAction: IndentAction.None, removeText: 1 }
+			},
+			{
+				// e.g.  *-----*/|
+				beforeText: /^(\t|( ))* \*[^/]*\*\/\s*$/,
+				action: { indentAction: IndentAction.None, removeText: 1 }
+			}
+		];
 
 	static readonly slashEnterRules: OnEnterRule[] = [
 		{
@@ -50,19 +56,21 @@ export class Rules
 		}
 	];
 
-	static readonly hashEnterRules: OnEnterRule[] = [
-		{
-			// e.g. # ...|
-			beforeText: /^\s*#/,
-			action: { indentAction: IndentAction.None, appendText: '# ' }
-		}
-	];
+	static readonly hashEnterRules: OnEnterRule[] =
+		[
+			{
+				// e.g. # ...|
+				beforeText: /^\s*#/,
+				action: { indentAction: IndentAction.None, appendText: '# ' }
+			}
+		];
 
-	static readonly semicolonEnterRules: OnEnterRule[] = [
-		{
-			// e.g. ; ...|
-			beforeText: /^\s*;/,
-			action: { indentAction: IndentAction.None, appendText: '; ' }
-		}
-	];
+	static readonly semicolonEnterRules: OnEnterRule[] =
+		[
+			{
+				// e.g. ; ...|
+				beforeText: /^\s*;/,
+				action: { indentAction: IndentAction.None, appendText: '; ' }
+			}
+		];
 }
