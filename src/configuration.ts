9cf28c0343be277ea25cba960ad37606fbb633db
derkallevombau <kalle@Aspire2>
2020-05-03 15:22:00 +0200

    configuration.ts:
    Improved formatting.
    
    Import singleLineConfig and multiLineConfig instead of deserialising JSON.
    
    Removed useless type specifiers.

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,185 +1,200 @@
-'use strict';
-
 import { Disposable, ExtensionContext, IndentAction, LanguageConfiguration, OnEnterRule, TextEditor, TextEditorEdit, commands, languages, workspace, } from 'vscode';
 
 import { Rules } from './rules';
+import { singleLineConfig } from 'language-configuration/single-line-configuration';
+import { multiLineConfig } from 'language-configuration/multi-line-configuration';
 
-let fs = require('fs');
+export class Configuration
+{
+	private readonly extensionName          = 'auto-comment-blocks';
+	private readonly singleLineBlockCommand = 'auto-comment-blocks.singleLineBlock';
 
-export class Configuration {
-  
-  private readonly extensionName: string = "auto-comment-blocks";
-  private readonly singleLineBlockCommand: string = 
-      "auto-comment-blocks.singleLineBlock";
-  private readonly singleLineConfigFile: string = __dirname +
-      "/../../language-configuration/single-line-configuration.json";
-  private readonly multiLineConfigFile: string = __dirname +
-      "/../../language-configuration/multi-line-configuration.json";
+	private readonly singleLineBlockOnEnter = 'singleLineBlockOnEnter';
+	private readonly slashStyleBlocks       = 'slashStyleBlocks';
+	private readonly hashStyleBlocks        = 'hashStyleBlocks';
+	private readonly semicolonStyleBlocks   = 'semicolonStyleBlocks';
+	private readonly disabledLanguages      = 'disabledLanguages';
 
-  private readonly singleLineBlockOnEnter: string = "singleLineBlockOnEnter";
-  private readonly slashStyleBlocks: string = "slashStyleBlocks";
-  private readonly hashStyleBlocks: string = "hashStyleBlocks";
-  private readonly semicolonStyleBlocks: string = "semicolonStyleBlocks";
-  private readonly disabledLanguages: string = "disabledLanguages";
+	private disabledLanguageList = this.getConfiguration().get<string[]>(this.disabledLanguages);
 
-  private disabledLanguageList: string[] = 
-      this.getConfiguration().get<string[]>(this.disabledLanguages);
-  private singleLineBlocksMap: Map<string, string> = new Map();
+	private singleLineStyleFromLangId = new Map<string, string>();
 
-  private getConfiguration() {
+	private getConfiguration()
+	{
+		return workspace.getConfiguration(this.extensionName);
+	}
 
-    return workspace.getConfiguration(this.extensionName);
-  }
+	private isLangIdDisabled(langId: string)
+	{
+		return this.disabledLanguageList.includes(langId);
+	}
 
-  private isLangIdDisabled(langId: string) {
+	private setLanguageConfiguration(langId: string, multiLine?: boolean, singleLineStyle?: string): Disposable
+	{
+		const langConfig: LanguageConfiguration =
+		{
+			onEnterRules: []
+		};
 
-    return this.disabledLanguageList.indexOf(langId) !== -1;
-  }
+		if (multiLine)
+		{
+			langConfig.onEnterRules = langConfig.onEnterRules.concat(Rules.multilineEnterRules);
+		}
 
-  private getMultiLineLanguages(): Array<string> {
+		const isOnEnter = this.getConfiguration().get<boolean>(this.singleLineBlockOnEnter);
 
-    let multiLineConfig = JSON.parse(fs.readFileSync(
-        this.multiLineConfigFile, 'utf-8'));
-    return multiLineConfig["languages"];
-  }
+		if (isOnEnter && singleLineStyle)
+		{
+			if (singleLineStyle === '//')
+			{
+				langConfig.onEnterRules = langConfig.onEnterRules.concat(Rules.slashEnterRules);
+			}
+			else if (singleLineStyle === '#')
+			{
+				langConfig.onEnterRules = langConfig.onEnterRules.concat(Rules.hashEnterRules);
+			}
+			else if (singleLineStyle === ';')
+			{
+				langConfig.onEnterRules = langConfig.onEnterRules.concat(Rules.semicolonEnterRules);
+			}
+		}
 
-  private setLanguageConfiguration(langId: string, 
-                                   multiLine?: boolean, 
-                                   singleLineStyle?: string): Disposable {
+		return languages.setLanguageConfiguration(langId, langConfig);
+	}
 
-    var langConfig: LanguageConfiguration = { 
-      onEnterRules: []
-    };
+	// Populates `this.singleLineStyleFromLangId`
+	private getSingleLineLanguages()
+	{
+		const commentStyles = Object.keys(singleLineConfig);
 
-    if (multiLine) {
-      langConfig.onEnterRules =
-          langConfig.onEnterRules.concat(Rules.multilineEnterRules);
-    }
+		for (const commentStyle of commentStyles)
+		{
+			for (const langId of singleLineConfig[commentStyle])
+			{
+				if (!this.isLangIdDisabled(langId))
+				{
+					this.singleLineStyleFromLangId.set(langId, commentStyle);
+				}
+			}
+		}
 
-    let isOnEnter = this.getConfiguration().get<boolean>(
-        this.singleLineBlockOnEnter);
-    if (isOnEnter && singleLineStyle) {
-      if (singleLineStyle === '//') {
-        langConfig.onEnterRules =
-            langConfig.onEnterRules.concat(Rules.slashEnterRules);
-      } else if (singleLineStyle === '#') {
-        langConfig.onEnterRules = 
-            langConfig.onEnterRules.concat(Rules.hashEnterRules);
-      } else if (singleLineStyle === ';') {
-        langConfig.onEnterRules =
-            langConfig.onEnterRules.concat(Rules.semicolonEnterRules);
-      }
-    }
-    
-    return languages.setLanguageConfiguration(langId, langConfig);
-  }
+		// Add user-customized langIds and comment styles to the map
 
-  private getSingleLineLanguages() {
+		const customSlashLangs = this.getConfiguration().get<string[]>(this.slashStyleBlocks);
 
-    let singleLineConfig: Object = JSON.parse(fs.readFileSync(
-      this.singleLineConfigFile, 'utf-8'));
-    let commentStyles = Object.keys(singleLineConfig);
-    for (let key of commentStyles) {
-      for (let langId of singleLineConfig[key]) {
-        if (!this.isLangIdDisabled(langId)) {
-          this.singleLineBlocksMap.set(langId, key);
-        }
-      }
-    }
-    
-    // get user-customized langIds for this key and add to the map
-    let customSlashLangs = 
-        this.getConfiguration().get<string[]>(this.slashStyleBlocks);
-    for (let langId of customSlashLangs) {
-      if (langId && langId.length > 0) {
-        this.singleLineBlocksMap.set(langId, '//');
-      }
-    }
+		for (const langId of customSlashLangs)
+		{
+			if (langId && langId.length > 0)
+			{
+				this.singleLineStyleFromLangId.set(langId, '//');
+			}
+		}
 
-    let customHashLangs = 
-        this.getConfiguration().get<string[]>(this.hashStyleBlocks);
-    for (let langId of customHashLangs) {
-      if (langId && langId.length > 0) {
-        this.singleLineBlocksMap.set(langId, '#');
-      }
-    }
+		const customHashLangs = this.getConfiguration().get<string[]>(this.hashStyleBlocks);
 
-    let customSemicolonLangs =
-        this.getConfiguration().get<string[]>(this.semicolonStyleBlocks);
-    for (let langId of customSemicolonLangs) {
-      if (langId && langId.length > 0) {
-        this.singleLineBlocksMap.set(langId, ';');
-      }
-    }
-  }
+		for (const langId of customHashLangs)
+		{
+			if (langId && langId.length > 0)
+			{
+				this.singleLineStyleFromLangId.set(langId, '#');
+			}
+		}
 
-  configureCommentBlocks(context: ExtensionContext) {
+		const customSemicolonLangs = this.getConfiguration().get<string[]>(this.semicolonStyleBlocks);
 
-    this.getSingleLineLanguages();
+		for (const langId of customSemicolonLangs)
+		{
+			if (langId && langId.length > 0)
+			{
+				this.singleLineStyleFromLangId.set(langId, ';');
+			}
+		}
+	}
 
-    // set language configurations
-    let multiLineLangs = this.getMultiLineLanguages();
-    for (let [langId, style] of this.singleLineBlocksMap) {
-      let multiLine = multiLineLangs.indexOf(langId) !== -1;
-      let disposable = this.setLanguageConfiguration(langId, multiLine, style);
-      context.subscriptions.push(disposable);
-    }
+	configureCommentBlocks(context: ExtensionContext)
+	{
 
-    for (let langId of multiLineLangs) {
-      if (!this.singleLineBlocksMap.has(langId) && 
-          !this.isLangIdDisabled(langId)) {
+		// Populate `this.singleLineStyleFromLangId`
+		this.getSingleLineLanguages();
 
-        let disposable = this.setLanguageConfiguration(langId, true);
-        context.subscriptions.push(disposable);
-      }
-    }
-  }
+		// Set language configurations
 
-  private handleSingleLineBlock(textEditor: TextEditor, edit: TextEditorEdit) {
+		const multiLineLangs = multiLineConfig['languages'];
 
-    let langId = textEditor.document.languageId;
-    var style = this.singleLineBlocksMap.get(langId);
-    if (style && textEditor.selection.isEmpty) {
-      let line = textEditor.document.lineAt(textEditor.selection.active);
-      let isCommentLine = true;
-      var indentRegex: RegExp;
-      if (style === '//' && line.text.search(/^\s*\/\/\s*/) !== -1) {
-        indentRegex = /\//;
-        if (line.text.search(/^\s*\/\/\/\s*/) !== -1) {
-          style = '///';
-        }
+		for (const [langId, style] of this.singleLineStyleFromLangId)
+		{
+			const multiLine = multiLineLangs.includes(langId);
 
-      } else if (style === '#' && line.text.search(/^\s*#\s*/) !== -1) {
-        indentRegex = /#/;
+			context.subscriptions.push(this.setLanguageConfiguration(langId, multiLine, style));
+		}
 
-      } else if (style === ';' && line.text.search(/^\s*;\s*/) !== -1) {
-        indentRegex = /;/;
+		for (const langId of multiLineLangs)
+		{
+			if (!this.singleLineStyleFromLangId.has(langId) && !this.isLangIdDisabled(langId))
+			{
+				context.subscriptions.push(this.setLanguageConfiguration(langId, true));
+			}
+		}
+	}
 
-      } else {
-        isCommentLine = false;
-      }
+	private handleSingleLineBlock(textEditor: TextEditor, edit: TextEditorEdit)
+	{
+		const langId = textEditor.document.languageId;
+		let   style  = this.singleLineStyleFromLangId.get(langId);
 
-      if (!isCommentLine) {
-        return;
-      }
+		if (style && textEditor.selection.isEmpty)
+		{
+			const line          = textEditor.document.lineAt(textEditor.selection.active);
+			let   isCommentLine = true;
+			let   indentRegex: RegExp;
 
-      var indentedNewLine = '\n' + 
-          line.text.substring(0, line.text.search(indentRegex));
-      let isOnEnter = this.getConfiguration().get<boolean>(
-          this.singleLineBlockOnEnter);
-      if (!isOnEnter) {
-        indentedNewLine += style + ' ';
-      }
+			if (style === '//' && line.text.search(/^\s*\/\/\s*/) !== -1)
+			{
+				indentRegex = /\//;
 
-      edit.insert(textEditor.selection.active, indentedNewLine);
-    }
-  }
+				if (line.text.search(/^\s*\/\/\/\s*/) !== -1)
+				{
+					style = '///';
+				}
 
-  registerCommands() {
+			}
+			else if (style === '#' && line.text.search(/^\s*#\s*/) !== -1)
+			{
+				indentRegex = /#/;
+			}
+			else if (style === ';' && line.text.search(/^\s*;\s*/) !== -1)
+			{
+				indentRegex = /;/;
+			}
+			else
+			{
+				isCommentLine = false;
+			}
 
-    commands.registerTextEditorCommand(this.singleLineBlockCommand,
-        (textEditor, edit, args) => {
-          this.handleSingleLineBlock(textEditor, edit);
-        })
-  }
+			if (!isCommentLine)
+			{
+				return;
+			}
+
+			let   indentedNewLine = '\n' + line.text.substring(0, line.text.search(indentRegex));
+			const isOnEnter       = this.getConfiguration().get<boolean>(this.singleLineBlockOnEnter);
+
+			if (!isOnEnter)
+			{
+				indentedNewLine += style + ' ';
+			}
+
+			edit.insert(textEditor.selection.active, indentedNewLine);
+		}
+	}
+
+	registerCommands()
+	{
+		commands.registerTextEditorCommand(this.singleLineBlockCommand,
+			(textEditor, edit, args) =>
+			{
+				this.handleSingleLineBlock(textEditor, edit);
+			}
+		);
+	}
 }
